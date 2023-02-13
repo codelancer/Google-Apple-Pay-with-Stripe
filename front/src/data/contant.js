aba8cf14ac909bb608eb389f73a8a0fa60ac0722
codelancer <124344659+codelancer@users.noreply.github.com>
2023-02-13 11:37:26 +0000

    Add files via upload

---
+++ front/src/data/contant.js
@@ -0,0 +1,8 @@
+//  stripe key
+export const STRIPE_PUBLIC_KEY='pk_test_51MZQ2AG5IGm3FoZQxkJ7CqzlMaaOvpkauL85YkHcgUhlw09QapT3SEg0bqb2OqHr5GXW23LIiQby5Dd0GFiiJkWU00wMNLOp2y';
+export const STRIPE_SECRET_KEY='sk_test_51MZQ2AG5IGm3FoZQu27XI5fIEy0ZXKDETQkXpQsGnuaSERpewtFZcpOkxNFHEzMFlMwtITvn4MOGAEr2txxjys0L00NdYmpngH';
+
+// paypal sandbox dev account ID
+export const SAND_ID = 'sb-l6mbr25025458@business.example.com';
+// pay amount
+export const PAY_AMOUNT = 999;
